391db9bdae1e10e8bd02e1851d9e26bfe77871a8
Rikisan <rimpa007@hotmail.com>
2018-11-15 23:36:07 -0600

    nueva funcion

--- 1_jueves.js
+++ 1_jueves.js
@@ -19,24 +19,25 @@ console.log(x);
 //crear un arreglo con los numeros del 1 al 10 revueltos y una funcion que los acomode
 //de manera descendente
 
-let arreglo = [2, 10, 7, 5, 4, 1, 3, 6, 8,9];
-console.log(`arreglo desordenado: ${arreglo}`);
+let arreglo1 = [2, 10, 7, 5, 4, 1, 3, 6, 8, 9];
+let arreglo2 = [2, 10, 7, 5, 4, 1, 3, 6, 8, 9];
+console.log(`arreglo desordenado         : ${arreglo1}`);
+arreglo1.sort(function(a,b){return b-a});
+console.log(`arreglo ordenado magicamente: ${arreglo1}`);
 
-let descendente = (arreglo) => {
-    for(i=0; i<arreglo.length; i++){
-        for(j=0; j<arreglo.length; j++){
-            if(arreglo[j]>arreglo[j+1]){
-                temp = arreglo[j];
-                arreglo[j] = arreglo[j+1];
-                arreglo[j+1] = temp
+let descendente = (arreglo2) => {
+    for(i=0; i<arreglo2.length; i++){
+        for(j=0; j<arreglo2.length; j++){
+            if(arreglo2[j]<arreglo2[j+1]){
+                temp = arreglo2[j];
+                arreglo2[j] = arreglo2[j+1];
+                arreglo2[j+1] = temp
             }
         }
     }
 }
-descendente(arreglo);
-console.log(`arreglo ordenado   : ${arreglo}`);
-
-
+descendente(arreglo2);
+console.log(`arreglo ordenado            : ${arreglo2}`);
 
 //crear arreglo planeta completa y magicamente matar a marte
 //crear un nuevo planeta despues de venus que se llama 'planeta Kachyz'
@@ -63,3 +64,11 @@ console.log(`Planetas Arnarquicos: ${planeta}`);
 //tomar el mismo arreglo del primer ejercicio, crear una funcion que te regrese un arreglo
 //del mismo tamaño donde cada numero es el cubo del numero original
 
+let arreglo3 = [2, 10, 7, 5, 4, 1, 3, 6, 8, 9];
+let cubo = (arreglo3) => {
+    for(i=0; i<arreglo3.length; i++){
+        arreglo3[i]=(arreglo3[i]**3);
+    }
+}
+cubo(arreglo3);
+console.log(`Arreglo, El cubo de cada elemento: ${arreglo3}`);
