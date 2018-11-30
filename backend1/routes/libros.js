57256b391d922759e365abde000668499b4f65e3
Rikisan <rimpa007@hotmail.com>
2018-11-29 19:25:35 -0600

    3 jueves

--- backend1/routes/libros.js
+++ backend1/routes/libros.js
@@ -1,4 +1,4 @@
-const express = require('express')
+onst express = require('express')
 const router = express.Router()
 const bodyParser = require('body-parser')
 
@@ -8,11 +8,9 @@ let libros = []
 
 /*
 {
-    titulo: String,
-    descripcion:  String,
-    anio : Number
-
-
+    "titulo": "String",
+    "descripcion":  "String",
+    "anio" : "Number"
 }
 */
 
@@ -27,7 +25,21 @@ router.post('/', (req, res) => {
     res.send(req.body)
 })
 
-router.delete('/', (req, res) => {
+router.delete('/:libros', (req, res) => { //envio el libro que quiero eliminar a http://localhost:3000/libros/<libro que quiero eliminar>
+    let index = -1;
+    let miLibro = req.params.libros       //guardo el libro recibido en una variable
+    for(let i=0; i < libros.length; i++) {
+        if(libros[i].titulo == miLibro) index = i;
+    }
+    if (index < 0)
+        res.status(400).send('Book not found')
+    else {
+        libros.splice(index, 1)
+        res.status(200).send(libros)
+    }
+  })
+
+  router.put('/', (req, res) => {
     let index = -1;
     for(let i=0; i < libros.length; i++) {
         if(libros[i].titulo == req.body.titulo) index = i;
@@ -35,7 +47,7 @@ router.delete('/', (req, res) => {
     if (index < 0)
         res.status(400).send('Book not found')
     else {
-        libros.splice(index, 1)
+        libros[index] = req.body
         res.status(200).send(libros)
     }
   })
